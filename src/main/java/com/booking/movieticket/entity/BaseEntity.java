ec2bc87809896dfaf7d83c0828bd1d6eb2285afe
Tung Nguyen Thanh 6 (SDC3.BU1) <tung.nguyen18@ntq-solution.com.vn>
2025-03-26 19:04:58 +0700

    feat: update source base + authen

---
+++ src/main/java/com/booking/movieticket/entity/BaseEntity.java
@@ -0,0 +1,52 @@
+package com.booking.movieticket.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLRestriction;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners( AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdBy", "createdAt", "lastModifiedBy", "lastModifiedAt"}, allowGetters = true)
+@SQLRestriction("is_deleted IS DISTINCT FROM true")
+public class BaseEntity implements Serializable
+{
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedBy
+    @Column(name = "last_modified_by")
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    @Column(name = "last_modified_at")
+    private Instant lastModifiedAt;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
+
+    @Column(name = "is_enabled")
+    private Boolean isEnabled;
+}
