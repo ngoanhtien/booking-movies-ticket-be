cccbde67a54c754f7de828e2c7013a9db8f629b8
ngoanhtien <ngoanhtien902@gmail.com>
2025-05-18 15:52:08 +0700

    add List

--- src/main/java/com/booking/movieticket/service/impl/BranchServiceImpl.java
+++ src/main/java/com/booking/movieticket/service/impl/BranchServiceImpl.java
@@ -27,6 +27,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Objects;
 
 @Service
